3875c4565246580f78f78c8359b004461d4137ad
jabber <2898117012@qq.com>
2022-07-04 01:58:48 +0800

    init

--- shopeo-cases.php
+++ shopeo-cases.php
@@ -13,6 +13,23 @@
  * Requires PHP: 5.6
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// Define SHOPEO_CASES_PLUGIN_FILE.
+if ( ! defined( 'SHOPEO_CASES_PLUGIN_FILE' ) ) {
+	define( 'SHOPEO_CASES_PLUGIN_FILE', __FILE__ );
+}
+
+if ( ! defined( 'SHOPEO_CASES_PLUGIN_BASE' ) ) {
+	define( 'SHOPEO_CASES_PLUGIN_BASE', plugin_basename( SHOPEO_CASES_PLUGIN_FILE ) );
+}
+
+if ( ! defined( 'SHOPEO_CASES_PATH' ) ) {
+	define( 'SHOPEO_CASES_PATH', plugin_dir_path( SHOPEO_CASES_PLUGIN_FILE ) );
+}
+
 if ( ! function_exists( 'shopeo_cases_activate' ) ) {
 	function shopeo_cases_activate() {
 
@@ -28,3 +45,11 @@ if ( ! function_exists( 'shopeo_cases_deactivate' ) ) {
 }
 
 register_deactivation_hook( __FILE__, 'shopeo_cases_deactivate' );
+
+if ( ! function_exists( 'shopeo_cases_load_textdomain' ) ) {
+	function shopeo_cases_load_textdomain() {
+		load_plugin_textdomain( 'shopeo-cases', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	}
+}
+
+add_action( 'init', 'shopeo_cases_load_textdomain' );
